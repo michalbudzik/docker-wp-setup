ce3e4d9fc9dc6b30feeed09ed495443f0ed4a8f7
Michał Budzik <michal@mindborn.pl>
2024-02-07 13:13:18 +0100

    creds update

--- cli/hide-env.sh
+++ cli/hide-env.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# truncate -s 0 ../creds/*.txt
-# sed 's/elo/xxx/g'
-# sed -i d %f
 sed "s/=.*/=/"
-# truncate -s 0 $1
--- cli/hide-secrets.sh
+++ cli/hide-secrets.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# truncate -s 0 ../creds/*.txt
-# sed 's/elo/xxx/g'
-# sed -i d %f
-# truncate -s 0 $1
 echo ''
